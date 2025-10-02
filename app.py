892e8f36c2991cd6e005105b6c5939ceea32d3e8
ilkeremiraydin9-sys <ilkeremiraydin9@gmail.com>
2025-10-02 22:48:46 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+import os
+
+app = Flask(__name__)
+
+@app.route("/health")
+def health():
+    return jsonify({"ok": True})
+
+@app.route("/")
+def home():
+    return "Shopify Bot Çalışıyor ✅"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
